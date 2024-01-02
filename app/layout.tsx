404a8ce544565cc5c91227c9e78965d6d4514db2
minoru horiuchi <noru238@gmail.com>
2024-01-02 10:12:07 +0900

    mantineを使用してみた

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
+import { ColorSchemeScript, MantineProvider } from '@mantine/core'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import '@mantine/core/styles.css'
+import { theme } from '@/app/libs/theme'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,7 +15,12 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <head>
+        <ColorSchemeScript />
+      </head>
+      <body className={inter.className}>
+        <MantineProvider theme={theme}>{children}</MantineProvider>
+      </body>
     </html>
   )
 }
