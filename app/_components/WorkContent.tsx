20334dda27674c8f4b7842746a315d54a1c4934c
minoru horiuchi <noru238@gmail.com>
2024-01-04 15:47:13 +0900

    worksリファクタリング

--- app/_components/WorkContent.tsx
+++ app/_components/WorkContent.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Card, CardBody, CardHeader, useDisclosure } from '@nextui-org/react'
+import { Card, CardBody, CardFooter, CardHeader, Chip, useDisclosure } from '@nextui-org/react'
 import { WorkContentModal } from '@/app/_components/WorkContentModal'
 import { WorkItemTop } from '@/app/_libs/type'
 
@@ -9,7 +9,7 @@ export function WorkContent({ key, item }: { key: string; item: WorkItemTop }) {
   return (
     <>
       <Card
-        className='relative w-full rounded-3xl bg-transparent py-10'
+        className='relative w-full rounded-3xl bg-transparent pb-2 pt-10'
         onPress={onOpen}
         isHoverable={true}
         isPressable={true}
@@ -19,9 +19,11 @@ export function WorkContent({ key, item }: { key: string; item: WorkItemTop }) {
         <CardHeader className='flex justify-center overflow-visible py-2 after:absolute after:left-1/2 after:top-10 after:-z-10 after:h-16 after:w-14 after:rounded after:bg-yellow-300/30 after:blur-sm dark:after:bg-yellow-400/10'>
           {item.icon}
         </CardHeader>
-        <CardBody className='flex-col items-start gap-2 px-4'>
-          <p className='text-lg font-bold'>{item.title}</p>
-          <small className='text-default-500'>{item.body}</small>
+        <CardBody className='flex-col items-start px-4'>
+          <p className='text-small text-default-500'> {item.term}</p>
+
+          <p className='flex items-center text-lg font-bold'>{item.title}</p>
+          <small className='mt-3 text-default-500'>{item.body}</small>
         </CardBody>
       </Card>
       <WorkContentModal item={item} isOpen={isOpen} onOpenChange={onOpenChange} />
